53f7899732f8b8a0f6176e13ac2350544e7e0c05
Wallace Gonçalves <wallacecosme@gmail.com>
2023-06-25 13:30:17 -0300

    Update opportunityController

--- report/src/main/java/org/br/mineradora/controller/OpportunityController.java
+++ report/src/main/java/org/br/mineradora/controller/OpportunityController.java
@@ -3,13 +3,11 @@ package org.br.mineradora.controller;
 import io.quarkus.security.Authenticated;
 import org.br.mineradora.dto.OpportunityDTO;
 import org.br.mineradora.service.OpportunityService;
-import org.eclipse.microprofile.jwt.JsonWebToken;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-
 import java.util.List;
 
 @Path("/api/opportunity")
@@ -17,14 +15,10 @@ import java.util.List;
 public class OpportunityController {
 
     @Inject
-    JsonWebToken token;
-
-    @Inject
     OpportunityService opportunityService;
 
     @GET
     @Path("/data")
-    @RolesAllowed({"user","manager"})
     public List<OpportunityDTO> generateReport(){
 
         return opportunityService.generateOpportunityData();
